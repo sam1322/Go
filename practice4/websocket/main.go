06aeafdc77f0bf8d4f4cceab876faefef499bced
sam1322 <sriram.pant@5gmail.com>
2023-09-08 21:12:42 +0530

    learnt websockets in go

--- practice4/websocket/main.go
+++ practice4/websocket/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+
 	"github.com/gorilla/websocket"
 )
 
@@ -9,6 +12,39 @@ var upgrader = websocket.Upgrader{
 	WriteBufferSize: 1024,
 }
 
-func main() {
+var clients = make(map[*websocket.Conn]bool)
 
+func main() {
+	http.HandleFunc("/echo", func(w http.ResponseWriter, r *http.Request) {
+		conn, _ := upgrader.Upgrade(w, r, nil)
+		clients[conn] = true
+
+		for {
+			msgType, msg, err := conn.ReadMessage()
+			if err != nil {
+				return
+			}
+			fmt.Printf("%s sent : %s and messageType : %v\n", conn.RemoteAddr(), string(msg), msgType)
+
+			// for returning the message to the same user connected
+			// if err = conn.WriteMessage(msgType, msg); err != nil {
+			// 	return
+			// }
+
+			// Broadcast the message to all connected clients
+			for client := range clients {
+				fmt.Println("client", client.RemoteAddr())
+				if err = client.WriteMessage(msgType, msg); err != nil {
+					// If there's an error sending to a client, remove them from the clients map
+					delete(clients, client)
+					client.Close()
+				}
+			}
+		}
+
+	})
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "websockets.html")
+	})
+	http.ListenAndServe(":8080", nil)
 }
