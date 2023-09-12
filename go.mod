a3b668ea96f80c51fbf0ad47cdf75058759a6e98
sri-vasitum <sriram.vasitum@gmail.com>
2023-09-12 22:56:12 +0530

    cleaned the http-server file

--- go.mod
+++ go.mod
@@ -3,9 +3,12 @@ module Go
 go 1.21.0
 
 require (
+	github.com/gorilla/handlers v1.5.1
+	github.com/gorilla/mux v1.8.0
+)
+
+require (
 	github.com/felixge/httpsnoop v1.0.1 // indirect
-	github.com/gorilla/handlers v1.5.1 // indirect
-	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/gorilla/securecookie v1.1.1 // indirect
 	github.com/gorilla/sessions v1.2.1 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
--- http-server/main.go
+++ http-server/main.go
@@ -24,18 +24,6 @@ import (
 	"github.com/gorilla/mux"
 )
 
-// type Response struct {
-// 	Message string `json:"message"`
-// }
-
-// func hello(w http.ResponseWriter,r *http.Request){
-// 	var resp =
-// }
-
-// func main() {
-// 	http.HandleFunc("/",hello)
-// }
-
 // Create a struct to represent your JSON data
 type Response struct {
 	Message string `json:"message"`
@@ -44,41 +32,6 @@ type Response struct {
 func enableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
 }
-
-// Then, at the start of the handleArticles function, add the following line of code:
-
-// func main() {
-//  // fmt.Println("Hello")
-//  http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
-//      // enableCors(&w)
-//      // Create a Response struct
-//      response := Response{Message: "successfull "}
-
-//      // Marshal the struct into JSON format
-//      jsonData, err := json.Marshal(response)
-//      if err != nil {
-//          http.Error(w, "Error creating JSON response", http.StatusInternalServerError)
-//          return
-//      }
-
-//      // Set the Content-Type header to application/json
-//      w.Header().Set("Content-Type", "application/json")
-//      w.Header().Set("Access-Control-Allow-Origin", "*")
-//      // Write the JSON data as the response
-//      w.Write(jsonData)
-
-//  })
-//  // corsHandler := cors.Default().Handler
-
-//  // // Wrap the CORS handler around your HTTP server
-//  // http.Handle("/", corsHandler)
-//  // Start the HTTP server on port 8080
-//  fmt.Println("Server is listening on port 8081...")
-//  err := http.ListenAndServe(":8081", nil)
-//  if err != nil {
-//      fmt.Println("Error:", err)
-//  }
-// }
 
 func main() {
 	// Create a new router using Gorilla Mux
@@ -104,23 +57,9 @@ func main() {
 		w.Write(jsonData)
 	})
 
-	// Create a new CORS handler
-	// c := cors.New(cors.Options{
-	// 	AllowedOrigins: []string{"*"}, // Change this to the specific origins you want to allow
-	// 	AllowedMethods: []string{"GET", "OPTIONS"},
-	// })
-
-	// // Use the CORS handler with the router
-	// handler := c.Handler(router)
 	credentials := handlers.AllowCredentials()
 
-	// Start the HTTP server on port 8080
+	// Start the HTTP server on port 8081
 	fmt.Println("Server is listening on port 8081...")
 	http.ListenAndServe(":8081", handlers.CORS(credentials)(router))
-	// http.ListenAndServe(":8081", nil)
-	// err := http.ListenAndServe(":8081", nil)
-	// err := http.ListenAndServe(":8081", handler)
-	// if err != nil {
-	// 	fmt.Println("Error:", err)
-	// }
 }
