1d6394dd8a127f01e5950cad0ec93be0b1f208a6
Sam <sriram.pant5@gmail.com>
2024-01-23 22:27:08 +0530

    was revising go concepts

--- Day1/hello/hello.go
+++ Day1/hello/hello.go
@@ -10,7 +10,56 @@ const s string = "constant"
 
 var r = "st"
 
+func sum(nums ...int) int {
+	total := 0
+	for _, num := range nums {
+		total += num
+	}
+	return total
+}
+
+func intSeq() func() int {
+	i := 0
+	return func() int {
+		i++
+		return i
+	}
+}
+
+func zeroval(ival int) {
+	ival = 0
+}
+func zeroptr(iptr *int) {
+	*iptr = 0
+}
+
 func main() {
+	testing3()
+}
+
+func testing3() {
+	i := 1
+	fmt.Println("initial ", i)
+
+	zeroval(i)
+	fmt.Println("zeroval ", i)
+
+	zeroptr(&i)
+	fmt.Println("zeroptr ", i, &i)
+}
+
+func testing2() {
+	nextInt := intSeq()
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+
+	newInts := intSeq()
+	fmt.Println(newInts())
+
+}
+
+func testing() {
 	fmt.Println(s, r)
 	fmt.Println("Hello, World!")
 	// fmt.Println(quote.Go())
@@ -22,11 +71,23 @@ func main() {
 	fmt.Println(d, n, int64(d), math.Sin(n))
 
 	fmt.Println(math.Pi)
+	str := make([]int, 3)
+	str2 := make([]int, 3)
+	// var ctr int = 0
+	ctr := 0
 	for i := 0; i < 10; i++ {
-		fmt.Print(rand.Intn(10), " ")
+		j := rand.Intn(10)
+		ctr += j
+		fmt.Print(j, " ")
+		str = append(str, j)
+		str2 = append(str2, ctr)
 	}
 	fmt.Println()
 	f := "Apple"
 	fmt.Println(f)
 
+	total := sum(str...)
+	fmt.Println("total sum = ", total)
+	fmt.Println(str)
+	fmt.Println(str2)
 }
--- go.mod
+++ go.mod
@@ -5,13 +5,13 @@ go 1.21.0
 require (
 	github.com/gorilla/handlers v1.5.1
 	github.com/gorilla/mux v1.8.0
+	github.com/gorilla/sessions v1.2.1
+	github.com/gorilla/websocket v1.5.0
+	github.com/lib/pq v1.10.9
+	golang.org/x/crypto v0.13.0
 )
 
 require (
 	github.com/felixge/httpsnoop v1.0.1 // indirect
 	github.com/gorilla/securecookie v1.1.1 // indirect
-	github.com/gorilla/sessions v1.2.1 // indirect
-	github.com/gorilla/websocket v1.5.0 // indirect
-	github.com/lib/pq v1.10.9 // indirect
-	golang.org/x/crypto v0.13.0 // indirect
 )
